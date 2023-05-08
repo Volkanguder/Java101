5d0e1da7a4f66dee6dbd62841c9064b45ce1ec90
volka <volkanguder@hotmail.com>
2023-05-08 23:39:01 +0300

    Hipotenus Hesaplama

---
+++ src/TemelKavramlar/Pratikler/_03_HipotenusBul/HipotenusBulanProgram.java
@@ -0,0 +1,22 @@
+package TemelKavramlar.Pratikler._03_HipotenusBul;
+
+import java.util.Scanner;
+
+public class HipotenusBulanProgram {
+    public static void main(String[] args) {
+        //Değişkenleri tanımlıyoruz. Ondalıklı girişe karşı double olarak ayarladım
+        double a, b, c;
+
+        //Kullanıcıdan veri alabilmek için Scanner sınıfı tanımlanır ve alınan veriler değişkenlere atanır
+        Scanner input = new Scanner(System.in);
+        System.out.print("1. dik kenarın uzunluğunu giriniz :");
+        a = input.nextDouble();
+        System.out.print("2. dik kenarın uzunluğunu giriniz :");
+        b = input.nextDouble();
+
+        //Kullanıcıdan alınan verileri hesaplama
+        c = Math.sqrt((a * a) + (b * b));
+        //Hipotenüs uzunluğunu ekrana yazdır
+        System.out.println("Hipotenüs uzunluğu : " + c);
+    }
+}
---
+++ src/TemelKavramlar/Pratikler/_03_HipotenusBul/README.md
@@ -0,0 +1,31 @@
+# Patika Java101
+```
+Program to Find the Hypotenuse of a Right Triangle
+Write a program in Java that takes the length of the legs of a right triangle from the user and calculates the hypotenuse.
+
+Assignment
+Write a program that takes the length of three sides of a triangle from the user and calculates its area.
+
+Formula
+Semiperimeter of the triangle = 2u
+
+u = (a+b+c) / 2
+
+Area * Area = u * (u - a) * (u - b) * (u - c)
+```
+
+```
+Dik Üçgende Hipotenüs Bulan Program
+Java ile kullanıcıdan dik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan programı yazın.
+
+Ödev
+Üç kenar uzunluğunu kullanıcıdan aldığınız üçgenin alanını hesaplayan programı yazınız.
+
+Formül
+Üç𝑔𝑒𝑛𝑖𝑛 ç𝑒𝑣𝑟𝑒𝑠𝑖 = 2𝑢
+
+𝑢 = (a+b+c) / 2
+
+Alan * Alan = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)
+```
+[Patika](https://academy.patika.dev/courses/java101)
