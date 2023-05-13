f80e84f3a9087bbaf2d3b9cc96a255172f21dbd1
volka <volkanguder@hotmail.com>
2023-05-13 18:26:21 +0300

    Girilen 3 sayıyı küçükten büyüğe sıralama

---
+++ src/KosulluIfadelerVeKodBloklari/Pratikler/_05_SayilariKucuktenBuyugeSiralama/README.md
@@ -0,0 +1,9 @@
+# Patika Java101
+```
+A program that sorts 3 entered numbers "from smallest to largest".
+```
+
+```
+Girilen 3 sayıyı "küçükten büyüğe" sıralayan program.
+```
+[Patika](https://academy.patika.dev/courses/java101)
--- src/KosulluIfadelerVeKodBloklari/Pratikler/_05_SayilariKucuktenBuyugeSiralama/SayilariKucuktenBuyugeSiralama.java
+++ src/KosulluIfadelerVeKodBloklari/Pratikler/_05_SayilariKucuktenBuyugeSiralama/SayilariKucuktenBuyugeSiralama.java
@@ -1,8 +1,8 @@
-package KosulluIfadelerVeKodBloklari.Pratikler._05_SayilariBuyuktenKucugeSiralama;
+package KosulluIfadelerVeKodBloklari.Pratikler._05_SayilariKucuktenBuyugeSiralama;
 
 import java.util.Scanner;
 
-public class SayilariBuyuktenKucugeSiralama {
+public class SayilariKucuktenBuyugeSiralama {
     public static void main(String[] args) {
         // Scanner sınıfı ile kullanıcıdan veri alınır.
         Scanner input = new Scanner(System.in);
@@ -10,10 +10,8 @@ public class SayilariBuyuktenKucugeSiralama {
         // kullanıcıdan üç sayı girişi alınır
         System.out.print("Lütfen ilk sayıyı giriniz: ");
         int num1 = input.nextInt();
-
         System.out.print("Lütfen ikinci sayıyı giriniz: ");
         int num2 = input.nextInt();
-
         System.out.print("Lütfen üçüncü sayıyı giriniz: ");
         int num3 = input.nextInt();
 
@@ -24,28 +22,28 @@ public class SayilariBuyuktenKucugeSiralama {
         // sıralama işlemi için üç sayı arasında karşılaştırma yapılır
         if (num1 < num2 && num1 < num3) { // num1 en küçük sayı ise
             smallest = num1;
-            if (num2 < num3) { // sayi2 ortanca, sayi3 en büyük sayı ise
+            if (num2 < num3) { // num2 ortanca, num3 en büyük sayı ise
                 middle = num2;
                 largest = num3;
-            } else { // sayi3 ortanca, sayi2 en büyük sayı ise
+            } else { // num3 ortanca, num2 en büyük sayı ise
                 middle = num3;
                 largest = num2;
             }
-        } else if (num2 < num1 && num2 < num3) { // sayi2 en küçük sayı ise
+        } else if (num2 < num1 && num2 < num3) { // num2 en küçük sayı ise
             smallest = num2;
-            if (num1 < num3) { // num1 ortanca, sayi3 en büyük sayı ise
+            if (num1 < num3) { // num1 ortanca, num3 en büyük sayı ise
                 middle = num1;
                 largest = num3;
-            } else { // sayi3 ortanca, num1 en büyük sayı ise
+            } else { // num3 ortanca, num1 en büyük sayı ise
                 middle = num3;
                 largest = num1;
             }
-        } else { // sayi3 en küçük sayı ise
+        } else { // num3 en küçük sayı ise
             smallest = num3;
-            if (num1 < num2) { // num1 ortanca, sayi2 en büyük sayı ise
+            if (num1 < num2) { // num1 ortanca, num2 en büyük sayı ise
                 middle = num1;
                 largest = num2;
-            } else { // sayi2 ortanca, num1 en büyük sayı ise
+            } else { // num2 ortanca, num1 en büyük sayı ise
                 middle = num2;
                 largest = num1;
             }
