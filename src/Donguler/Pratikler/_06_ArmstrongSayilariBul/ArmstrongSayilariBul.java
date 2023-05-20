700d22a435a4ecae5424645e3083e6862899f069
volka <volkanguder@hotmail.com>
2023-05-21 02:08:50 +0300

    Armstrong Sayılarını bulan Program

--- src/Donguler/Pratikler/_06_ArmstrongSayilariBul/ArmstrongSayilariBul.java
+++ src/Donguler/Pratikler/_06_ArmstrongSayilariBul/ArmstrongSayilariBul.java
@@ -10,18 +10,25 @@ public class ArmstrongSayilariBul {
         System.out.print("Bir sayı girin : ");
         int sayi = input.nextInt();
 
+        // Sayının orijinal değerini tutmak için bir değişken oluşturulur
         int orjinalSayi = sayi;
+        // Basamakların toplamını tutmak için bir değişken oluşturulur
         int toplam = 0;
 
         while (sayi != 0) {
+            // Sayının son basamağı bulunur
             int basamak = sayi % 10;
+            // Basamak toplama eklenir
             toplam += basamak;
+            // Sayıda son basamak çıkarılarak güncellenir
             sayi /= 10;
         }
 
         System.out.println("Girilen sayının basamakları toplamı : " + toplam);
 
+        // Armstrong sayısı için basamakların üçüncü kuvvetlerinin toplamını tutmak için bir değişken oluşturulur
         int armstrongToplam = 0;
+        // Sayıyı tekrar orjinal değeriyle günceller
         sayi = orjinalSayi;
 
         while (sayi != 0) {
